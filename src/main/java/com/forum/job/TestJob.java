303ecfbd89d5e48aac534cc33a441acc10f154e4
onedaymore <andayfour@gmail.com>
2018-06-20 19:41:00 +0800

    springboot 默认日志配置

--- src/main/java/com/forum/job/TestJob.java
+++ src/main/java/com/forum/job/TestJob.java
@@ -6,20 +6,19 @@ import org.springframework.stereotype.Component;
 
 import com.forum.util.DateUtils;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class TestJob {
 
 	private int i = 1;
 	public void get() {
-		System.out.println("开始测试");
+		log.info("任务测试开始");
 		String currentTime = DateUtils.formatDate(new Date(), "yyyy-MM-dd HH:mm:ss");
+		log.warn("获取时间成功");
 		String s = "第" + (i++) + "次";
-		System.out.println( s + currentTime);
-	}
-	
-	
-	public static void main(String[] args) {
-		int i=2;
-		System.out.println("di" + ++i +"s" );
+		log.error("获取次数成功");
+		log.debug( s + currentTime);
 	}
 }
